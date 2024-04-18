f1579dcb131331f44c70798ac2dbd18960f489bd
John Lukich <john.p.lukich@gmail.com>
2024-04-18 06:42:38 -0600

    docs metadata

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routers import collections, items, users
+from tags import tags_metadata
 
-app = FastAPI()
+app = FastAPI(
+    title='Collections',
+    description='A place to track and share things you collect',
+    contact={
+        'name': 'John Lukich',
+        'url': 'http://john-lukich.mooo.com',
+    },
+    openapi_tags=tags_metadata,
+)
 
 app.include_router(collections.router)
 app.include_router(items.router)
---
+++ api/tags.py
@@ -0,0 +1,14 @@
+tags_metadata = [
+    {
+        'name': 'collections',
+        'description': 'CRUD operations for \'collections\''
+    },
+    {
+        'name': 'items',
+        'description': 'CRUD operations for \'items\''
+    },
+    {
+        'name': 'users',
+        'description': '\'user\' operations, including **login** and **token** logic'
+    }
+]
